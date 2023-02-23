fcca600b5cb00e4daa62f31d51f040d893d7a828
ZaitonZz <philipcandelario1@gmail.com>
2023-02-23 17:50:28 +0800

    added basic option in search, updated onBackPressed and made the program welcome user

--- app/src/main/java/com/example/codegreen/View/MainScreenActivity.java
+++ app/src/main/java/com/example/codegreen/View/MainScreenActivity.java
@@ -1,16 +1,40 @@
 package com.example.codegreen.View;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.codegreen.R;
+import com.example.codegreen.data.Model.CurrentUser;
 
 public class MainScreenActivity extends AppCompatActivity {
 
+    TextView welcomeMessage;
+    Button searchButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        searchButton = findViewById(R.id.buttonSearch);
+        welcomeMessage = findViewById(R.id.welcomeText);
+        String s = "dsa";
+        welcomeMessage.setText("Welcome "+CurrentUser.getUsername() +"!");
+
+        searchButton.setOnTouchListener((view, motionEvent) -> {
+            startActivity(new Intent(MainScreenActivity.this, SearchActivity.class));
+            return false;
+        });
+    }
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(true);
+        android.os.Process.killProcess(android.os.Process.myPid());
+        System.exit(0);
     }
 }
--- app/src/main/java/com/example/codegreen/View/SearchActivity.java
+++ app/src/main/java/com/example/codegreen/View/SearchActivity.java
@@ -1,16 +1,36 @@
 package com.example.codegreen.View;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.codegreen.R;
 
 public class SearchActivity extends AppCompatActivity {
+    Spinner spinnerAreas, spinnerProduce;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        spinnerAreas=findViewById(R.id.spinnerAreas);
+        spinnerProduce = findViewById(R.id.spinnerProduce);
+
+        ArrayAdapter<CharSequence>adapter= ArrayAdapter.createFromResource(this, R.array.Areas, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinnerAreas.setAdapter(adapter);
+
+        adapter= ArrayAdapter.createFromResource(this, R.array.Produce, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinnerProduce.setAdapter(adapter);
+    }
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(true);
+        android.os.Process.killProcess(android.os.Process.myPid());
+        System.exit(0);
     }
 }
