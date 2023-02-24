ec788073ece481ed888403ba2ffef2a986711b67
HP <dan04arevalo@gmail.com>
2023-02-24 17:45:00 +0800

    add map feature and linked map and search buttons to map and search activity. problem: map won't load if button is clicked.

--- app/src/main/java/com/example/codegreen/View/MainScreenActivity.java
+++ app/src/main/java/com/example/codegreen/View/MainScreenActivity.java
@@ -6,11 +6,47 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.codegreen.R;
 
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
+
 public class MainScreenActivity extends AppCompatActivity {
+    private Button mapButton;
+    private Button searchButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+
+        mapButton = (Button) findViewById(R.id.button2);
+        mapButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                openMaps();
+            }
+        });
+
+        searchButton = (Button) findViewById(R.id.button);
+        searchButton.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSearch();
+            }
+        }));
+
+    }
+
+    public void openMaps() {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
+
+    public void openSearch() {
+        Intent intent = new Intent(this, SearchActivity.class);
+        startActivity(intent);
     }
 }
